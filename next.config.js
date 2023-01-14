35987a45c3c35fe75f484817fa2361924fbf2e78
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2023-01-14 18:19:48 +0100

    feat : added eslint scss prettier

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('node:path')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
+module.exports = nextConfig
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import '@/../styles/Home.module.scss';
+
+export default function Home() {
+    return (
+            <>
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
+            </>
+    )
+}
